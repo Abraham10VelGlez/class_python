d7083f9c7cffd143d4926b9f77ed3f7b00bb7c2b
Abraham10VelGlez <usbrifa@gmail.com>
2025-03-07 14:33:54 -0600

    en_preparacionclase1

---
+++ main.py
@@ -0,0 +1,97 @@
+print('hola mundo')
+
+dictamen_nobre = 'Empresa de datos del StudioA'
+print(dictamen_nobre)
+
+entero = 10
+
+decimal = 10.501
+
+boolean_verdadero=True
+
+boolean_falso=False
+
+#esttructturas
+listaconindice = [ 15, 30 , 45 , 55 , 80 , 100]
+
+print(listaconindice[5])
+#listas de texto o listas mixtas
+
+
+
+
+
+#diccionario de numero a texto
+avaluos = {
+    100 : 'Dictamen/Bimbo/000100',
+    104 : 'Dictamen/Bimbo/000100',
+    108 : 'Dictamen/Bimbo/000100'
+}
+print(avaluos[108])
+#diccionario de  texto a texto
+
+#diccionario de  texto a listas
+
+
+#constantes
+#su valor no puede ser cambiantes
+Pi= 3.1416
+print(Pi)
+
+
+#Operadores simbolos para hacer operaciones + - * /
+print(5+5)
+print(10+5)
+print(5-5)
+print(5/5)
+
+#Operadores comparativos
+#print(5==5)
+#print(10 == '10')
+#print(5 != 10)
+#print(100>=10)
+#print(20<=10)
+
+
+#operadores logicos , util en condicionales
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+
+
+acceso_autorizado_al_codiminio = True
+
+
+if acceso_autorizado_al_codiminio:
+    print('Puede acceder al condominio')
+else:
+    print('No puede acceder al condominio')
+
+       
+
+valor = 10000
+
+if valor == 1000:
+    print('son iguales')
+elif valor <= 15:
+    print('no es menor a 15 el numero{valor}')
+else:
+    print('este numero es mayor de 1000')
+    
+    
+    
+cadena_txt = 'Tipologia'
+#comparacion de varios casos
+match 
+    
+    
+
+
+
+    
+
+
+
+
